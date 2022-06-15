317a0d66e2fb53101107d3175e0689f2dc9fb8bb
Zhao Jun <leo.zhaojun@gmail.com>
2022-06-15 18:00:25 +0800

    remove pixi interaction

--- src/index.js
+++ src/index.js
@@ -86,8 +86,4 @@ export {
   destroyAndCleanAllCache
 } from "./utils/cache";
 
-export {
-  enableInteraction,
-} from "./utils/interaction";
-
 global.InkPaint = exports;
--- src/utils/interaction.js
+++
@@ -1,9 +0,0 @@
-import { InteractionManager, interactiveTarget } from '@pixi/interaction';
-import { WebGLRenderer, CanvasRenderer, DisplayObject } from '../core';
-import { mixin } from './mixin';
-
-export function enableInteraction() {
-  CanvasRenderer.registerPlugin('interaction', InteractionManager);
-  WebGLRenderer.registerPlugin('interaction', InteractionManager);
-  mixin(DisplayObject.prototype, interactiveTarget);
-}
