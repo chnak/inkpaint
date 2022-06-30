f877152d0e1851a2e917568957b1259310388700
Zhao Jun <leo.zhaojun@gmail.com>
2022-06-30 16:18:32 +0800

    add comment of blend modes

--- src/const.js
+++ src/const.js
@@ -8,26 +8,26 @@ export const RENDERER_TYPE = {
 };
 
 export const BLEND_MODES = {
-  NORMAL: 0,
-  ADD: 1,
-  MULTIPLY: 2,
-  SCREEN: 3,
-  OVERLAY: 4,
-  DARKEN: 5,
-  LIGHTEN: 6,
-  COLOR_DODGE: 7,
-  COLOR_BURN: 8,
-  HARD_LIGHT: 9,
-  SOFT_LIGHT: 10,
-  DIFFERENCE: 11,
-  EXCLUSION: 12,
-  HUE: 13,
-  SATURATION: 14,
-  COLOR: 15,
-  LUMINOSITY: 16,
-  NORMAL_NPM: 17,
-  ADD_NPM: 18,
-  SCREEN_NPM: 19
+  NORMAL: 0, // 正常
+  ADD: 1, // 线性加深(添加)
+  MULTIPLY: 2, // 正片叠底
+  SCREEN: 3, // 滤色
+  OVERLAY: 4, // 叠加
+  DARKEN: 5, // 变暗
+  LIGHTEN: 6, // 变亮
+  COLOR_DODGE: 7, // 颜色减淡
+  COLOR_BURN: 8, // 颜色加深
+  HARD_LIGHT: 9, // 强光
+  SOFT_LIGHT: 10, // 柔光
+  DIFFERENCE: 11, // 差值
+  EXCLUSION: 12, // 排除: 与“差值”模式相似但对比度更低的效果
+  HUE: 13, // 色相: 用基色的明亮度和饱和度以及混合色的色相创建结果色
+  SATURATION: 14, // 饱和度: 用基色的明亮度和色相以及混合色的饱和度创建结果色
+  COLOR: 15, // 颜色: 用基色的明亮度以及混合色的色相和饱和度创建结果色
+  LUMINOSITY: 16, // 明度: 用基色的色相和饱和度以及混合色的明亮度创建结果色
+  NORMAL_NPM: 17, // webgl的NORMAL, not-premultiplied, only for webgl
+  ADD_NPM: 18, // webgl的ADD, not-premultiplied, only for webgl
+  SCREEN_NPM: 19 // webgl的SCREEN, not-premultiplied, only for webgl
 };
 
 export const DRAW_MODES = {
